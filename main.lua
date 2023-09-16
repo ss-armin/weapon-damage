64b4f286d283041e5ec77da95d0d5643e22e6c18
A R M I N <123939474+ss-armin@users.noreply.github.com>
2023-09-16 11:36:56 +0330

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,151 @@
+-- unarmed damage
+CreateThread(function()
+    while true do
+	N_0x4757f00bc6323cfe(GetHashKey("WEAPON_UNARMED"), 0.01) 
+	Wait(0)
+    end
+end)
+
+-- bat damage
+CreateThread(function()
+    while true do
+	N_0x4757f00bc6323cfe(GetHashKey("WEAPON_BAT"), 0.1) 
+	Wait(0)
+    end
+end)
+
+-- dagger damage
+CreateThread(function()
+    while true do
+	N_0x4757f00bc6323cfe(GetHashKey("WEAPON_DAGGER"), 0.1) 
+	Wait(0)
+    end
+end)
+
+-- bottle damage
+CreateThread(function()
+    while true do
+	N_0x4757f00bc6323cfe(GetHashKey("WEAPON_BOTTLE"), 0.1) 
+	Wait(0)
+    end
+end)
+
+-- crowbar damage
+CreateThread(function()
+    while true do
+	N_0x4757f00bc6323cfe(GetHashKey("WEAPON_CROWBAR"), 0.1) 
+	Wait(0)
+    end
+end)
+
+-- flashlight damage
+CreateThread(function()
+    while true do
+	N_0x4757f00bc6323cfe(GetHashKey("WEAPON_FLASHLIGHT"), 0.1) 
+	Wait(0)
+    end
+end)
+
+-- golfclub damage
+CreateThread(function()
+    while true do
+	N_0x4757f00bc6323cfe(GetHashKey("WEAPON_GOLFCLUB"), 0.1) 
+	Wait(0)
+    end
+end)
+
+-- hammer damage
+CreateThread(function()
+    while true do
+	N_0x4757f00bc6323cfe(GetHashKey("WEAPON_HAMMER"), 0.1) 
+	Wait(0)
+    end
+end)
+
+-- hatchet damage
+CreateThread(function()
+    while true do
+	N_0x4757f00bc6323cfe(GetHashKey("WEAPON_HATCHET"), 0.1) 
+	Wait(0)
+    end
+end)
+
+-- knuckles damage
+CreateThread(function()
+    while true do
+	N_0x4757f00bc6323cfe(GetHashKey("WEAPON_KNUCKLE"), 0.1) 
+	Wait(0)
+    end
+end)
+
+-- knife damage
+CreateThread(function()
+    while true do
+	N_0x4757f00bc6323cfe(GetHashKey("WEAPON_KNIFE"), 0.1) 
+	Wait(0)
+    end
+end)
+
+-- machete damage
+CreateThread(function()
+    while true do
+	N_0x4757f00bc6323cfe(GetHashKey("WEAPON_MACHETE"), 0.1) 
+	Wait(0)
+    end
+end)
+
+-- switchblade damage
+CreateThread(function()
+    while true do
+	N_0x4757f00bc6323cfe(GetHashKey("WEAPON_SWITCHBLADE"), 0.1) 
+	Wait(0)
+    end
+end)
+
+-- nightstick damage
+CreateThread(function()
+    while true do
+	N_0x4757f00bc6323cfe(GetHashKey("WEAPON_NIGHTSTICK"), 0.1) 
+	Wait(0)
+    end
+end)
+
+-- wrench damage
+CreateThread(function()
+    while true do
+	N_0x4757f00bc6323cfe(GetHashKey("WEAPON_WRENCH"), 0.1) 
+	Wait(0)
+    end
+end)
+
+-- battleaxe damage
+CreateThread(function()
+    while true do
+	N_0x4757f00bc6323cfe(GetHashKey("WEAPON_BATTLEAXE"), 0.1) 
+	Wait(0)
+    end
+end)
+
+-- poolcue damage
+CreateThread(function()
+    while true do
+	N_0x4757f00bc6323cfe(GetHashKey("WEAPON_POOLCUE"), 0.1) 
+	Wait(0)
+    end
+end)
+
+-- stone_hatchet damage
+CreateThread(function()
+    while true do
+	N_0x4757f00bc6323cfe(GetHashKey("WEAPON_STONE_HATCHET"), 0.1) 
+	Wait(0)
+    end
+end)
+
+-- candycane damage
+CreateThread(function()
+    while true do
+	N_0x4757f00bc6323cfe(GetHashKey("WEAPON_CANDYCANE"), 0.1) 
+	Wait(0)
+    end
+end)
